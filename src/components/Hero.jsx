970e81b4233ac4df3328c9a6e8585bcd7c4e50b3
Usman Munir <usmanmunir1008@gmail.com>
2024-10-19 00:39:39 +0500

    hero and nav (not final)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,16 @@ const Hero = () => {
       className="flex flex-col text-white justify-center items-center h-screen bg-cover bg-center"
       style={{ backgroundImage: `url(${bgImage})` }}
     >
-      <div className="">
-        <h1>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-bold bg-[#384D3A] p-4 m-4 rounded-lg">
           Welcome to <span className="text-[#f39c12]">Rapid Kurier</span>
         </h1>
-        <p>Reliable, Fast, and Affordable Delivery Solutions</p>
-        <button className="bg-yellow-500">Our Services</button>
+        <p className="text-lg py-2">
+          Reliable, Fast, and Affordable Delivery Solutions
+        </p>
+        <button className="bg-[#f39c12] p-2 m-2 rounded-md">
+          Our Services
+        </button>
       </div>
     </section>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
   return (
     <nav className="flex justify-between items-center mx-8">
       <img className="cursor-pointer h-28 w-32" src={logo} alt="logo image" />
-      <ul className="flex">
+      <ul className="md:flex hidden">
         <li className="px-4 py-2 cursor-pointer capitalize font-medium text-[#417c4c] hover:scale-105 hover:text-[#033003] duration-150">
           Home
         </li>
