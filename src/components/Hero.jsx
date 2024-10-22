591974d4924e0e7e3e43c88d32cc6ba5b388179d
Usman Munir <usmanmunir1008@gmail.com>
2024-10-22 22:22:04 +0500

    responsive ui

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,20 +5,24 @@ import { IoMdArrowDropright } from "react-icons/io";
 
 const Hero = () => {
   return (
-    <section className="flex text-white justify-center items-center text-center h-[82vh]">
-      <div className="flex flex-col justify-center items-start p-8 w-1/2">
-        <h1 className="text-4xl font-bold bg-[#384D3A] p-4 rounded-lg">
+    <section className="flex flex-col min-h-[50vh] h-auto text-white justify-center items-center text-center p-4 md:flex-row lg:h-[82vh] md:p-0 2xl:pl-12">
+      <div className="flex flex-col justify-center items-center md:items-start md:p-8 md:w-[50%]">
+        <h1 className="text-4xl font-bold bg-[#384D3A] text-center md:text-left p-4 rounded-lg 2xl:text-6xl">
           Welcome to <span className="text-[#f39c12]">Rapid Kurier</span>
         </h1>
-        <p className="text-lg text-black p-3">
+        <p className="text-lg text-black py-3 xl:text-2xl text-left">
           Reliable, Fast, and Affordable Delivery Solutions
         </p>
-        <button className="flex items-center bg-[#f39c12] text-lg py-2 px-4 m-2 rounded-md group hover:bg-[#e67e22] cursor-pointer">
-          Our Services
+        <button className="absolute top-[55%] md:static flex items-center bg-[#f39c12] text-lg xl:text-xl py-2 px-4 rounded-md group hover:bg-[#e67e22] cursor-pointer">
+          OUR SERVICES
           <IoMdArrowDropright className="text-2xl transition-transform duration-300 group-hover:rotate-90" />
         </button>
       </div>
-      <img src={banner} alt="" className="w-1/2 h-2/3" />
+      <img
+        src={banner}
+        alt=""
+        className="md:w-[50%] h-auto max-h-full xl:right-0"
+      />
     </section>
   );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="flex justify-between bg-[#f0f0f0] shadow-md items-center px-4 md:px-6  lg:px-12 h-[18vh]">
+    <nav className="flex justify-between z-10 bg-[#f0f0f0] shadow-md items-center px-4 md:px-6  lg:px-12 h-[18vh] 2xl:px-16 xl:text-xl 2xl:text-2xl">
       <img className="cursor-pointer h-28 w-32" src={logo} alt="logo image" />
 
       <ul className="md:flex hidden">
