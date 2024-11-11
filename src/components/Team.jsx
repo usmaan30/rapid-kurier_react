94a6240ff36e3232f6f6e7e88e806ef1a4aadf8c
Usman Munir <usmanmunir1008@gmail.com>
2024-11-11 23:41:12 +0500

    .

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -31,7 +31,7 @@ const Team = () => {
             className="rounded-full border-4 border-solid border-[#333] w-[180px] h-[180px]"
           />
           <h4 className="mt-[10px] mb-[5px] font-semibold">John Doe</h4>
-          <p>CEO</p>
+          <p>COO</p>
         </div>
       </div>
     </section>
